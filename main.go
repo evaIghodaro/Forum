9f05e0f53fe8eb5b74c01074d787cbbdcd62b71f
ieva <eva.ighodaro@ynov.com>
2024-07-04 10:53:16 +0200

    ajout du liens serv

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
 	auth "forum/internal/authentification"
 	filters "forum/internal/filters"
 	moderation "forum/internal/moderation"
 	notification "forum/internal/notification"
-	"log"
-	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// Call setupDatabase function from setup_database.go
+	// Initialiser la base de données
 	setupDatabase()
 
 	db, err := sql.Open("sqlite3", "./forum.db")
@@ -27,10 +29,46 @@ func main() {
 	moderation.Initialize(db)
 	notification.Initialize(db)
 
+	// Gestionnaires pour les pages d'authentification
 	http.HandleFunc("/register", auth.RegisterHandler)
 	http.HandleFunc("/login", auth.LoginHandler)
-	// Ajoutez d'autres gestionnaires ici
+	http.HandleFunc("/logout", auth.LogoutHandler)
+
+	// Gestionnaires pour les pages du forum
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/categories", categoriesHandler)
+	http.HandleFunc("/post", postHandler)
+	http.Handle("/create_post", auth.AuthMiddleware(http.HandlerFunc(createPostHandler)))
+	http.HandleFunc("/profile", profileHandler)
+
+	// Middleware pour protéger certaines routes
+	http.Handle("/protected", auth.AuthMiddleware(http.HandlerFunc(protectedHandler)))
 
 	log.Println("Starting server on :8081")
+	fmt.Println("Server is running at http://localhost:8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "internal/templates/index.html")
+}
+
+func categoriesHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "internal/templates/categories.html")
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "internal/templates/post.html")
+}
+
+func createPostHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "internal/templates/create_post.html")
+}
+
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "internal/templates/profile.html")
+}
+
+func protectedHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a protected page")
 }
